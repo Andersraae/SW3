0b71db4a9d9f2f03bc5cdf4c569896890721e3c5
Anders <andersraae01@gmail.com>
2023-09-06 16:05:57 +0200

    Java fungerer ish

---
+++ OOP/GoodToKnow.java
@@ -0,0 +1,7 @@
+// package OOP; //  Java tror det er en normal folder og ikke en package... undersøg
+
+public class GoodToKnow {
+    public static void main(String[] args) {
+        System.out.println("Java tror det er en normal folder og ikke en package... undersøg");
+    }
+}
