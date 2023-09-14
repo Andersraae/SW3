81afab0fc4677851d5c42d6db9cd563dd0b40045
Anders <andersraae01@gmail.com>
2023-09-14 12:45:19 +0200

    OOP setup

--- OOP/GoodToKnow.java
+++
@@ -1,7 +0,0 @@
-// package OOP; //  Java tror det er en normal folder og ikke en package... undersøg
-
-public class GoodToKnow {
-    public static void main(String[] args) {
-        System.out.println("Java tror det er en normal folder og ikke en package... undersøg");
-    }
-}
