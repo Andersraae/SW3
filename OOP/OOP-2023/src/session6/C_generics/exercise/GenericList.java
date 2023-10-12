e43151e5a16eb6e6738ea621796cc7c04d35e4db
Anders <andersraae01@gmail.com>
2023-10-12 13:53:03 +0200

    session 6

--- OOP/OOP-2023/src/session6/C_generics/exercise/GenericList.java
+++ OOP/OOP-2023/src/session6/C_generics/exercise/GenericList.java
@@ -9,15 +9,15 @@ import session6.C_generics.Giraff;
  *  a) Run the following code and see why the current implementation of a "List" does not provide type safety.
  *  b) Use generics to adapt the following implementation of a "List" to provide type safety.
  */
-public class GenericList {
-    private Object[] array;
+public class GenericList<T> {
+    private T[] array;
     int count=0;
 
     public GenericList() {
-        array = new Object[100];
+        array = (T[]) new Object[100];
     }
 
-    public  void add(Object o) {
+    public  void add(T o) {
         array[count++]=o;
     }
 
@@ -27,15 +27,14 @@ public class GenericList {
 
     public static void main(String[] args) {
 
-        GenericList genericList = new GenericList();
+        GenericList genericList = new GenericList<>();
 
         Giraff giraff = new Giraff();
         genericList.add(giraff);
 
-        Book book = (Book) genericList.get(0);
+        Giraff giraff1 = (Giraff) genericList.get(0);
 
         /***** UNCOMMENT BELOW TO PROVIDE FEEDBACK ****/
-        /*
         //Did you have time to finish the exercise?
         boolean I_did_finish = true;
 
@@ -46,6 +45,5 @@ public class GenericList {
         String comment = "No comments";
 
         feedback.Feedback.submit("session6_C", I_did_finish, I_did_understand, comment);
-        */
     }
 }
